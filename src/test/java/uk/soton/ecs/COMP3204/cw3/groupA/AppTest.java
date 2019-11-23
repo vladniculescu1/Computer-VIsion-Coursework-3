a4032666dc7e04d55023748a2242091f00fe7d96
Vlad <danvladadrian@yahoo.com>
2019-11-23 18:55:24 +0000

    Initial commit

---
+++ src/test/java/uk/soton/ecs/COMP3204/cw3/groupA/AppTest.java
@@ -0,0 +1,18 @@
+package uk.soton.ecs.COMP3204.cw3.groupA;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
